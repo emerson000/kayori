ce262f42ce8469319f3eea92b15090e678afde9b
emerson000 <emerson@centercad.com>
2025-02-07 22:08:28 -0500

    feat: Add optional dependency for Tailwind CSS and update RSS feed creation to post tasks

--- services/frontend/src/app/collect/rss/actions.ts
+++ services/frontend/src/app/collect/rss/actions.ts
@@ -8,7 +8,19 @@ export async function createRssFeed(formData: FormData) {
     const urls = formData.getAll('urls[]') as string[];
     
     console.log({ title, urls });
-    fetch('/api/rss');
+    await fetch('/api/task', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: "12345",
+            service: "rss",
+            task: {
+                urls: urls
+            }
+        })
+    });
 
     redirect('/collect/rss');
 }
