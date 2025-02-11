e84cef2061671a0084bbf8793d3b5b3f8316b5db
emerson000 <94085544+emerson000@users.noreply.github.com>
2025-02-10 20:46:50 -0500

    fix: Filter out empty URLs when creating RSS feed

--- services/frontend/src/app/collect/rss/actions.ts
+++ services/frontend/src/app/collect/rss/actions.ts
@@ -5,7 +5,7 @@ import { postTask } from "../../../utils/shared";
 
 export async function createRssFeed(formData: FormData) {
     const title = formData.get('title') as string;
-    const urls = formData.getAll('urls[]') as string[];
+    const urls = (formData.getAll('urls[]') as string[]).filter((url) => url.length > 0);
     const jobId = await postTask('rss', title, { urls });
     redirect(`/collect/${jobId}`);
 }
