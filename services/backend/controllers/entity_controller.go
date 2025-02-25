4b0e4668308d3d525df0754b193c0d9b6158357c
emerson000 <94085544+emerson000@users.noreply.github.com>
2025-02-25 18:22:00 -0500

    refactor: backend controllers

---
+++ services/backend/controllers/entity_controller.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"kayori.io/backend/models"
+)
+
+func CreateNewsArticle(db *mongo.Database, rdb *redis.Client) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var article models.NewsArticle
+		if err := c.BodyParser(&article); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		article.EntityType = "news_article"
+		var existingArticle models.NewsArticle
+		if err := existingArticle.Read(context.Background(), db, "artifacts", bson.M{"checksum": article.Checksum}, &existingArticle); err == nil {
+			return c.Status(fiber.StatusConflict).JSON(fiber.Map{
+				"error": "Article already exists",
+			})
+		}
+
+		if err := article.Create(context.Background(), db, "artifacts", &article); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		articleData, err := json.Marshal(article)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		if err := rdb.Publish(context.Background(), "drone-status", articleData).Err(); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		return c.Status(fiber.StatusCreated).JSON(article)
+	}
+}
+
+func GetNewsArticles(db *mongo.Database) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		articles := make([]models.NewsArticle, 0)
+		if err := (&models.NewsArticle{}).ReadAll(context.Background(), db, "artifacts", bson.M{"entity_type": "news_article"}, &articles); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		return c.JSON(articles)
+	}
+}
---
+++ services/backend/controllers/job_controller.go
@@ -0,0 +1,181 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/segmentio/kafka-go"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"kayori.io/backend/models"
+)
+
+func CreateJob(db *mongo.Database, kafkaWriter *kafka.Writer) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var job models.Job
+		if err := c.BodyParser(&job); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		if err := job.Create(context.Background(), db, "jobs", &job); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		if job.Status == "" {
+			job.Status = "pending"
+		}
+
+		jobData, err := json.Marshal(job)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		err = kafkaWriter.WriteMessages(context.Background(),
+			kafka.Message{
+				Value: jobData,
+			},
+		)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		return c.JSON(job)
+	}
+}
+
+func GetJobs(db *mongo.Database) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		jobs := make([]models.Job, 0)
+		collection := db.Collection("jobs")
+		findOptions := options.Find().SetSort(bson.D{{Key: "title", Value: 1}})
+		cursor, err := collection.Find(context.Background(), bson.D{}, findOptions)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		defer cursor.Close(context.Background())
+
+		if err := cursor.All(context.Background(), &jobs); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		return c.JSON(jobs)
+	}
+}
+
+func GetJobByID(db *mongo.Database) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		objID, err := bson.ObjectIDFromHex(id)
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Invalid job ID",
+			})
+		}
+		collection := db.Collection("jobs")
+		var result bson.M
+		if err := collection.FindOne(context.TODO(), bson.M{"_id": objID}).Decode(&result); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		log.Printf("result: %+v\n", result["_id"])
+		var job models.Job
+		log.Printf("job ID: %+v\n", id)
+		if err := job.Read(context.Background(), db, "jobs", bson.M{"_id": objID}, &job); err != nil {
+			if err == mongo.ErrNoDocuments {
+				return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+					"error": "Job not found",
+				})
+			}
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		return c.JSON(job)
+	}
+}
+
+func UpdateJob(db *mongo.Database) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var job models.Job
+		if err := c.BodyParser(&job); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		id := c.Params("id")
+		objID, err := bson.ObjectIDFromHex(id)
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Invalid job ID",
+			})
+		}
+		job.ID = objID
+		updateStatement := bson.M{
+			"$set": bson.M{},
+		}
+		if job.Title != "" {
+			updateStatement["$set"].(bson.M)["title"] = job.Title
+		}
+		if job.Status != "" {
+			updateStatement["$set"].(bson.M)["status"] = job.Status
+		}
+		if err := job.Update(context.Background(), db, "jobs", updateStatement); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		err = job.Read(context.Background(), db, "jobs", bson.M{"_id": job.ID}, &job)
+		if err != nil {
+			return err
+		}
+		return c.JSON(job)
+	}
+}
+
+func GetJobArtifacts(db *mongo.Database) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		var artifacts = make([]bson.D, 0)
+		collection := db.Collection("artifacts")
+		objID, err := bson.ObjectIDFromHex(id)
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Invalid job ID",
+			})
+		}
+		cursor, err := collection.Find(context.Background(), bson.M{"job_id": objID})
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		if err := cursor.All(context.Background(), &artifacts); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		for i := range artifacts {
+			for j := range artifacts[i] {
+				if artifacts[i][j].Key == "_id" {
+					artifacts[i][j].Key = "id"
+				}
+			}
+		}
+
+		return c.JSON(artifacts)
+	}
+}
