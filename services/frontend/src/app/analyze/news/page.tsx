51f4649338631cef72b92a4f83ab75372e2ea4ef
emerson000 <94085544+emerson000@users.noreply.github.com>
2025-03-05 07:56:39 -0500

    fix: encode search parameter in pagination links

--- services/frontend/src/app/analyze/news/page.tsx
+++ services/frontend/src/app/analyze/news/page.tsx
@@ -7,21 +7,22 @@ export default async function Page({ searchParams }: { searchParams: Promise<{ [
     const { page = '1', search } = await searchParams;
     const pageNumber = parseInt(page as string);
     const articles = await getNews(pageNumber, search as string);
+    const searchEncoded = encodeURIComponent(search as string);
     return (
         <div>
             <h1 className="text-2xl font-bold">News</h1>
             <SearchBar search={search as string} />
             <div className="clear-both">
-                {pageNumber > 1 && <Link className="btn float-left" href={`?page=${pageNumber - 1}${search != "" ? `&search=${search}` : ''}`}>Previous</Link>}
-                <Link className="btn float-right" href={`?page=${pageNumber + 1}${search != "" ? `&search=${search}` : ''}`}>Next</Link>
+                {pageNumber > 1 && <Link className="btn float-left" href={`?page=${pageNumber - 1}${search ? `&search=${searchEncoded}` : ''}`}>Previous</Link>}
+                <Link className="btn float-right" href={`?page=${pageNumber + 1}${search ? `&search=${searchEncoded}` : ''}`}>Next</Link>
             </div>
             {articles && articles.length > 0 && <div className="clear-both mt-15">
                 {articles && articles.map((article, index) => <MessageCard message={article} key={index} />)}
             </div>}
             {articles && articles.length == 0 && <div className="text-center clear-both py-10 bg-base-200 mt-15 mb-5">No results found</div>}
             <div className="clear-both mb-20">
-                {pageNumber > 1 && <Link className="btn float-left" href={`?page=${pageNumber - 1}${search != "" ? `&search=${search}` : ''}`}>Previous</Link>}
-                <Link className="btn float-right" href={`?page=${pageNumber + 1}${search != "" ? `&search=${search}` : ''}`}>Next</Link>
+                {pageNumber > 1 && <Link className="btn float-left" href={`?page=${pageNumber - 1}${search ? `&search=${searchEncoded}` : ''}`}>Previous</Link>}
+                <Link className="btn float-right" href={`?page=${pageNumber + 1}${search ? `&search=${searchEncoded}` : ''}`}>Next</Link>
             </div>
         </div>
     );
